22039940574b4a06d2377e24f05bf87f43f431d3
Shraman Das <shraman99@gmail.com>
2020-06-08 12:49:15 +0530

    Just started. Simple Node example.

---
+++ NodeJS/node-examples/index.js
@@ -0,0 +1,23 @@
+var rect=
+{
+    perimeter:(l,b) => (2*(l+b)),
+    area : (l,b) => (l*b)
+};
+var square=
+{
+    perimeter:(side) => 4*side,
+    area:(side) => side*side 
+}
+
+function calcrect(l,b)
+{
+    if(l>0 && b>0)
+    {console.log("Solving rectangle perimeter"+rect.perimeter(l,b));
+    console.log("Solving rectangle area"+rect.area(l,b));}
+    else
+    console.log("Negative values? really bro? ");
+}
+
+calcrect(2,4);
+calcrect(-1,3);
+calcrect(17,13);
